cab70b6799d74c28cd879862c26efd99e85a7b62
Angeline Tamayo <tamayo.angeline@gmail.com>
2022-11-08 16:54:51 -0500

    :tada: Initial commit

---
+++ src/Main.elm
@@ -0,0 +1,67 @@
+module Main exposing (..)
+
+import Browser
+import Html exposing (Html, br, button, div, text)
+import Html.Events exposing (onClick)
+
+
+
+-- MAIN
+
+
+main =
+  Browser.sandbox { init = init, update = update, view = view }
+
+
+
+-- MODEL
+
+
+type alias Model = Tetromino
+
+type Tetromino
+    = I
+    | O
+
+init : Model
+init =
+  I
+
+
+
+-- UPDATE
+
+
+type Msg
+  = NewPiece
+
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    NewPiece ->
+      O
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ showPiece model
+        , button [ onClick NewPiece ] [ text "New Piece." ]
+        ]
+
+
+showPiece : Model -> Html Msg
+showPiece model =
+    case model of
+        I ->
+            div [] [ text "■■■■" ]
+
+        O ->
+            div [] [ text "■■"
+                   , br [] []
+                   , text "■■" ]
